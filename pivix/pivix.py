6dfd43c8fab5da9db2962dd27c272d9b861ac8ca
PurpleK <2kpurple@gmail.com>
2015-10-11 01:14:14 +0800

    添加p站代码

--- pivix/pivix.py
+++ pivix/pivix.py
@@ -1,14 +1,21 @@
 # coding: utf-8
 import sys
 import json
+import requests
 from requests import Session
 
 
 class Pixiv:
+
+    MODE_DAILY = 'daily'
+    MODE_WEEK = 'week'
+    MODE_MONTH = 'month'
+
     user = ''
     password = ''
     session = Session()
     login_url = 'https://www.secure.pixiv.net/login.php'
+    rank_url = 'http://www.pixiv.net/ranking.php'
 
     def __init__(self):
         config = self._load_config()
@@ -18,24 +25,28 @@ class Pixiv:
             self._login()
 
     def _login(self):
-        resp = self.session.get(self.login_url)
+        # resp = requests.get(self.login_url)
         post_data = {
             'mode': 'login',
             'pixiv_id': self.user,
             'pass': self.password,
             'skip': 1
         }
-        login_resp = self.session.post(self.login_url, data=post_data, headers=self.get_header(), cookies=resp.cookies)
-        print login_resp.text
+        self.session.post(self.login_url, data=post_data)
 
-    def get_header(self):
+    def _get_header(self):
         header = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/44.0.2403.157 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko)'
+                          ' Chrome/45.0.2454.101 Safari/537.36',
             'Referer': self.login_url,
             'Host': 'www.secure.pixiv.net'
         }
         return header
+
+    def get_daily_record(self, mode):
+        resp = self.session.get(url='http://www.pixiv.net/ranking.php?mode=daily')
+        print resp.text
+
 
     @staticmethod
     def _load_config():
@@ -56,5 +67,6 @@ class Pixiv:
             print 'No user or password in config file!'
         return None
 
-p = Pixiv()
-
+if __name__ == '__main__':
+    p = Pixiv()
+    p.get_daily_record(Pixiv.MODE_DAILY)
