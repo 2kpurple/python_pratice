249961a49dbe0afe6b51cc3c9691b31a97c02b6a
2kpurple <2kpurple@gmail.com>
2015-11-04 01:02:48 +0800

    完善炉石爬虫代码

--- HearthStone/hearthstonecard.py
+++ HearthStone/hearthstonecard.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 import json
 import requests
 import re
@@ -23,25 +25,40 @@ def handleCard(item):
     soup = BeautifulSoup(content, 'lxml')
     card_attr = soup.find_all('div',class_='card-attribute')[0]
     name = re.compile('\s*(.*)\s*').findall(card_attr.h2.a.string)[0]
-    # print card_attr.p
-    text_str = str(card_attr.p)
-    text_str = re.sub('<.>|<\/.>', '', text_str)
-    # print type(text_str)
-    print text_str
-    # card_text = re.compile()
-    # print name
     item['name_zh'] = name
-    item['text_zh'] = text_str
+    # print item['id']
+    print name
+    # print '-----------'
+    # print name
+    if not 'class' in card_attr.p.attrs:
+            text_str = str(card_attr.p)
+            text_str = unicode(text_str, 'utf-8')
+            text_str = re.sub('<[^>]*>|', '', text_str)
+            # print type(text_str)
+            item['text_zh'] = text_str
+            print text_str
+            # print type(text_str)
+            # print text_str
+    # print item
     # print type(card_attr)
 
     # print card_attr + "\n"
+
+def jsonWrite(js):
+    print js
+    json_str = json.dumps(js)
+    f = open('hearthstone_data_zh.json', 'wb')
+    json_str = json_str
+    # print type(json_str)
+    # print json_str
+    f.write(json_str)
 
 def main():
     js = getJson()
     for i in js:
         item = js[i]
         handleCard(item)
-    # print js
+    jsonWrite(js)
 
 if __name__ == '__main__':
     main();
